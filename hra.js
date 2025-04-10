e5dc2c46316c04ccdbacb7e365c707dd9a2bcfbc
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-04-10 11:05:26 +0200

    Fix: Implement player class toggle and player icon switch

--- hra.js
+++ hra.js
@@ -1,4 +1,45 @@
-let currentPlayer = 'circle'
+'use scrict'
+//skuska spojenia
+console.log("fungujemen - nefungujeme? fungujeme! :)")
 
-const playingCells = document.querySelectorAll("button")
-playingCells.add
+//vytvorim premennu hraca
+let currentPlayer = 'circle';
+
+//vyberiem vsetky policka z gridu
+const playingCells = document.querySelectorAll("button");
+
+//skontrolujem ci mam vybratych vsetkych 100 policok :)
+console.log(playingCells.length)
+
+//vyberiem ikonu hraca
+const player = document.querySelector('.game-board__player-icon img')
+
+
+
+//vybrate bunky prechadzam funkciou forEach, kde na kazdy jeden prvkov(cell) pridavam eventListener a zaznamenam  pomocou event.targetu (vlastnosti objedktu udalosti) referenciu bunky na ktoru bolo kliknute  a tej pomocou metody classList menim pridavam triedu- kruh
+playingCells.forEach((cell) => {
+    cell.addEventListener('click', (evt) => {
+        if (currentPlayer === 'circle') {
+            //zaroven to vnaram do podmienky- v ktorej zabezpecujem zmenu hraca cez dodobratie a pridanie tried
+            evt.target.classList.add("board__field--cross");
+            evt.target.classList.remove("board__field--circle");
+
+        } else if (currentPlayer === 'cross') {
+            evt.target.classList.add("board__field--circle");
+            evt.target.classList.remove("board__field--cross");
+        }
+
+
+        //zmena hraca na hornej ikone - pridam podmienku- ak klikne= zahra hrac s kruhom, tak sa zmeni aktualny hrac na kriz a opacne
+        if (currentPlayer === 'circle') {
+            currentPlayer = 'cross';
+            player.src = "/pictures/circle.svg";
+
+        } else if (currentPlayer === 'cross') {
+            currentPlayer = 'circle';
+            player.src = "/pictures/cross.svg";
+        }
+        console.log(currentPlayer);
+    })
+});
+
