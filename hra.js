473510a2fcd55b5dd45d4dfc8e9e7b1ca531067e
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-05-04 13:11:00 +0200

    feat: Added functionality to detect and announce winner and tie game

--- hra.js
+++ hra.js
@@ -1,4 +1,6 @@
-'use scrict'
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
+'use strict'
 //skuska spojenia
 console.log("fungujemen - nefungujeme? fungujeme! :)")
 
@@ -6,13 +8,56 @@ console.log("fungujemen - nefungujeme? fungujeme! :)")
 let currentPlayer = 'circle';
 
 //vyberiem vsetky policka z gridu
-const playingCells = document.querySelectorAll("button");
+const playingCells = document.querySelectorAll(".game-board__cell");
 
 //skontrolujem ci mam vybratych vsetkych 100 policok :)
 console.log(playingCells.length)
 
 //vyberiem ikonu hraca
 const player = document.querySelector('.game-board__player-icon img')
+
+
+// Ziskavam hodnoty zo vsetkych policok, tak ze si zmenim NodeList, ktory som ziskala z querryselectorall na Array, na ktore potom spustim map, cim ziskam nove pole, ktore obsahu hodnoty kazdej bunky
+
+
+
+const checkGameStatus = () => {
+    const valuePlayingCell = Array.from(playingCells).map(cell => {
+        if (cell.classList.contains("board__field--circle")) {
+            return 'o';
+        } else if (cell.classList.contains("board__field--cross")) {
+            return 'x';
+        } else {
+            return '_';
+        }
+
+    })
+
+    // kontrola vytvoreneho pola
+    console.log(valuePlayingCell)
+    console.log('Typy hodnôt v poli:', valuePlayingCell.map((v) => typeof v));
+
+
+    // stanovenie vitaza
+    const winner = findWinner(valuePlayingCell);
+    if (winner === 'o' || winner === 'x') {
+        setTimeout(() => {
+            alert(`Vyhrál hráč se symbolem ${winner}.`);
+            location.reload(); // voliteľné
+        }, 200);
+    } else if (winner === 'tie') {
+        setTimeout(() => {
+            alert("Hra skončila remízou!");
+            location.reload(); // voliteľné, aby sa hra reštartovala po remíze
+        }, 200);
+    }
+    // Ak hra ešte neskončila
+    else {
+        console.log("Hra pokračuje, ešte nie je víťaz.");
+    }
+};
+
+
 
 
 
@@ -33,6 +78,7 @@ playingCells.forEach((cell) => {
         }
 
 
+
         //zmena hraca na hornej ikone - pridam podmienku- ak klikne= zahra hrac s kruhom, tak sa zmeni aktualny hrac na kriz a opacne
         if (currentPlayer === 'circle') {
             currentPlayer = 'cross';
@@ -42,9 +88,12 @@ playingCells.forEach((cell) => {
             currentPlayer = 'circle';
             player.src = "pictures/circle.svg";
         }
+        checkGameStatus();
         console.log(currentPlayer);
     })
 });
+
+
 
 
 //bezpecnostne opatrenie
